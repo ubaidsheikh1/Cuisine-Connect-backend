177d7b4cf685cce28498a986f3b29b9eb45625e0
ubaidsheikh <us731506@gmail.com>
2024-04-28 10:09:33 +0530

    adding user profile feature

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,7 +1,8 @@
 import { Request,Response,NextFunction } from "express";
 import { body, validationResult } from "express-validator";
 
-const handleValidationErrors = async (req: Request, 
+const handleValidationErrors = async (
+    req: Request, 
     res: Response,
      next: NextFunction
     ) =>{
@@ -23,4 +24,28 @@ export const validateMyUserRequest = [
     body("city").isString().notEmpty().withMessage("City must be a string"),
     body("country").isString().notEmpty().withMessage("Country must be a string"),
     handleValidationErrors,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City is required"),
+  body("country").notEmpty().withMessage("Country is required"),
+  body("deliveryPrice")
+  .isFloat({min: 0})
+  .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+  .isInt({min: 0})
+  .withMessage("Estimated delivery time must be a positive integer"),
+  body("cuisines")
+  .isArray()
+  .withMessage("Cuisines must be an array")
+  .not()
+  .isEmpty()
+  .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.name")
+  .isFloat({min: 0})
+  .withMessage("Menu item price is required and must be a positive number"),
+  handleValidationErrors,
 ];
